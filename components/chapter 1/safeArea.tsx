705b323bd29ea41fe1544e79ebb59f10b56c3aad
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-14 09:26:16 +0200

    react-naitve: with tailwind

--- components/chapter 1/safeArea.tsx
+++ components/chapter 1/safeArea.tsx
@@ -4,7 +4,7 @@ import React,{ useState } from "react";
 function SafeArea() {
     const [count, setCount] = useState(0);
     return (
-        <SafeAreaView className="">
+        <SafeAreaView className="min-h-screen bg">
             <Text>Hello in safe area !</Text>
             <Text>{count}</Text>
 
