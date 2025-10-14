8120611d4375c297cdde72e32769b3935d5a0fd4
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-14 09:40:12 +0200

    react-naitve: with tailwind

--- components/chapter 1/safeArea.tsx
+++ components/chapter 1/safeArea.tsx
@@ -1,27 +1,17 @@
 // ? safeAreaView doesn't overlap with notches, status bar, or rounded corners iOS/Android 
-import { SafeAreaView, Text, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import React,{ useState } from "react";
 function SafeArea() {
     const [count, setCount] = useState(0);
     return (
-        <SafeAreaView className="min-h-screen bg-blue-500">
-            
-            <Text>Hello in safe area !</Text>
-            <Text>{count}</Text>
+   <View className="h-screen bg-blue-200 flex flex-col items-center justify-center">
+      <Text className="text-white text-2xl mb-4">Hello Full Screen!</Text>
+      <Text className="text-white text-xl mb-4">{count}</Text>
 
-            <Button 
-              title="+"
-              onPress={() => setCount(count + 1)}
-               />
-            <Button 
-              title="0"
-              onPress={() => setCount(0)}
-               />
-            <Button 
-              title="-"
-              onPress={() => setCount(count - 1)}
-               />
-        </SafeAreaView>
+      <Button title="+" onPress={() => setCount(count + 1)} />
+      <Button title="0" onPress={() => setCount(0)} />
+      <Button title="-" onPress={() => setCount(count - 1)} />
+    </View>
     )
 }
 
