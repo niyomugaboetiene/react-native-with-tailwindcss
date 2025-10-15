1895fc99e8f2af24e81fb49f78fa43edc9c783a6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-15 08:05:51 +0200

    react-native: integrating tailwindcss

--- components/chapter 1/textInput.tsx
+++ components/chapter 1/textInput.tsx
@@ -6,9 +6,9 @@ export default function TextInputs() {
 
   return (
     <View className="w-full bg-green-300 min-h-screen flex-1">
-      <Text className=""></Text>
+      <Text className="">Type Something</Text>
         <TextInput 
-        className="bg-gray-200 pt-4 mt-6 me-[200px] pe-4 ms-4 px-5 py-2 w-[330px] "
+        className="bg-gray-200 pt-4 mt-6 me-[200px] pe-4 ms-4 px-5 py-2 w-[330px] rounded-lg shadow-lg placeholder:font-bold"
            placeholder="Enter something..."
            onChangeText={setName}
     
