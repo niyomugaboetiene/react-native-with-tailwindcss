2cb27174badbe425279976cd54673d3e64bf8b4c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-15 10:30:22 +0200

    react-native: implementing simple project

--- components/chapter 1/proj.tsx
+++ components/chapter 1/proj.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { View, Text, TextInput } from "react-native"
+import { Button } from "react-native";
 
 function Project() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState(""); 
+    const [pressed, setPressed] = useState(false);
     return (
         <View>
             <Text>FIll out fo empy fields</Text>
@@ -26,6 +28,19 @@ function Project() {
               onChangeText={setPassword}
               placeholder="Type you password here"
               />
+
+              <Button title="Submit"
+                  onPress={() => setPressed(true)}
+            />
+
+            {pressed ? (
+                <>
+                   <Text>Hello you name is {name}</Text>
+                   <Text>Password {name}</Text>
+                </>
+            ) : (
+
+            )}
         </View>
     )
 }
