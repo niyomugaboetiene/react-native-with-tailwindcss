370f1123fbda61eb35459580ac5fd9cb01ebe57e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:29:06 +0200

    styling the simple project

--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,17 @@ import { ScreenContent } from '@/components/ScreenContent';
 import Interaction from '@/components/navigation/interaction';
 import Buttons from '@/components/chapter 1/button';
 import Project from '@/components/chapter 1/proj';
+import Ints from '@/components/navigation/project/ints';
 
 export default function Home() {
   return (
     <View className="flex-1">
       <Container>
         <ScreenContent>
-          <Interaction />
+          {/* <Interaction /> */}
           {/* <Project /> */}
            {/* <Buttons /> */}
+           <Ints />
         </ScreenContent>
       </Container>
     </View>
