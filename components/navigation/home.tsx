0468de5ce10cb28d997084934b18a79e41022f90
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:11:50 +0200

    styling the simple project

--- components/navigation/home.tsx
+++ components/navigation/home.tsx
@@ -5,10 +5,10 @@ import { ScreenContent } from '@/components/ScreenContent';
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-white">
+    <View className="flex-1 bg-blue-300 items-center justify-center">
       <Container>
         <ScreenContent>
-          <Text className="text-2xl font-bold">Home Screen</Text>
+          <Text className="text-2xl font-bold mt-[350px] text-white">Home Screen</Text>
           <Text className="text-gray-600">Welcome to the home tab!</Text>
         </ScreenContent>
       </Container>
