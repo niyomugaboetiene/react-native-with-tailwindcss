5bdd047d518f174115ae95573d6b54597f6c4e5c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:24:58 +0200

    styling the simple project

--- components/navigation/project/setting.tsx
+++ components/navigation/project/setting.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native"
 
-export default function Profile() {
+export default function Settings() {
     return (
         <View>
-            <Text>my profile App</Text>
+            <Text>You can manage your preference</Text>
         </View>
     )
 }
