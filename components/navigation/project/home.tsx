54f8f011bf23f34f34ad17e8013d7d17b18aa437
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:23:27 +0200

    styling the simple project

--- components/navigation/project/home.tsx
+++ components/navigation/project/home.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native"
 export default function HomeScreen() {
     return (
         <View>
-            
+            <Text>This is home app</Text>
         </View>
     )
 }
