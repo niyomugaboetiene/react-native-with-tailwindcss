b7dd2e8191cb6f93fedffc22dafc260e96be8b1e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:57:39 +0200

    styling the simple pro

--- components/navigation/home.tsx
+++ components/navigation/home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <View className="flex-1 bg-blue-300 items-center justify-center">
    
-          <Text className="text-2xl font-bold mt-[350px] text-white">Home Screen</Text>
+          <Text className="text-2xl font-bold mt-[30px] text-white">Home Screen</Text>
           <Text className="text-gray-600">Welcome to the home tab!</Text>
     
     </View>
