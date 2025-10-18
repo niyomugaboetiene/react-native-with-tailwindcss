bbdc5a1500a8d9dafb63c9ad80d42b72119ee053
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-18 11:56:53 +0200

    styling the simple pro

--- components/navigation/project/home.tsx
+++ components/navigation/project/home.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native"
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>This is home app</Text>
+        <View className="bg-lbue-400">
+            <Text className="text-center font-bold">This is home app</Text>
         </View>
     )
 }
